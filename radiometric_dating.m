1d0d1dc102a332b59f58fa7a36bebd26af213d98
Taha Altar Çağ <altarcag@gmail.com>
2023-10-29 00:00:15 +0300

    Update radiometric_dating.m

--- radiometric_dating.m
+++ radiometric_dating.m
@@ -4,13 +4,25 @@ x = dataset(:,1);
 y = dataset(:,2);
 p = polyfit(x,y,1);
 f = polyval(p,x);
-
 l = 10^11 / 1.42;    % lambda value for Rubidium
 m = p(1);
+y_i = p(2);  % the y-intersect value for initial isotope ratio
 t = log(m + 1) * l;  % Rb/Sr age of the sample
 
 fprintf('Slope (m) = %f\n', m); % slope
 fprintf('age (t) = %e\n', t);   % age
 
 plot(x,y,'o',x,f,'-')
-legend('data','linear fit')
+legend('data','linear fit', 'Location', 'southeast')
+
+% annotations for slope, age, and the initial isotope ratio(s)
+slope_annotation = sprintf('Slope (m) = %.4f', m);
+age_annotation = sprintf('Age (t) = %.4e', t);
+y_i_annotation = sprintf('Initial 87Sr/86Sr = %.4f', y_i);
+text(20, 0.98 * max(y), slope_annotation, 'FontSize', 12);
+text(20, 0.91 * max(y), age_annotation, 'FontSize', 12);
+text(20, 0.85 * max(y), y_i_annotation, 'FontSize', 12);
+
+% adding labels to the x and y axes
+xlabel('87Rb/86Sr');
+ylabel('87Sr/86Sr');
